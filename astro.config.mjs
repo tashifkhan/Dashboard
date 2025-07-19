d232a2b533bc1ff418c857974f347b93f5b18b08
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-07-20 05:11:24 +0530

    added sitemap in the build process

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tashif.codes/', // <-- Top-level, not inside sitemap()
   vite: {
     plugins: [tailwindcss()]
   },
-
-  integrations: [react()]
+  integrations: [
+    react(),
+    sitemap()
+  ]
 });
